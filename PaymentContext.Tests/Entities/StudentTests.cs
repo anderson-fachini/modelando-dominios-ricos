6757a987878ecf589583b7e19923d437ae9a28c5
Anderson Fachini <anderson.dkfachini@gmail.com>
2021-03-01 09:00:48 -0300

    Implementando validações

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.ValueObjects;
 
 namespace PaymentContext.Tests
 {
@@ -9,6 +10,11 @@ namespace PaymentContext.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            var name = new Name("Anderson", "Fachini");
+            foreach (var not in name.Notifications)
+            {
+                not.Message;
+            }
             //var subscription = new Subscription(null);
             //var student = new Student("Anderson", "Fachini", "123", "anderson.dkfachini@gmail.com");
             //student.AddSubscription(subscription);
